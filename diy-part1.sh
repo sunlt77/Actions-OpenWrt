b092b090dd92c36a57a7a0f63db5d584e2efc570
sunlt77 <103977593+sunlt77@users.noreply.github.com>
2026-06-21 15:01:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,11 @@
 # 移除openwrt feeds过时的luci版本
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
+# 强制移除官方 feeds 中的旧版本 SmartDNS 源码目录
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+
+# 克隆官方最新主分支源码（支持最新 WebUI）
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
